717acd130c8f0e98f043114e4c0d51576d54a2b0
roseeen <rneilrosen@gmail.com>
2024-02-21 12:37:03 +0800

    final commit

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -6,5 +6,19 @@ use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
-    //
+    public function show()
+    {
+        return view('Product')
+            ->with('Product1', 'Samsung')
+            ->with('Product2', 'Apple')
+            ->with('Product3', 'Huawei')
+            ->with('Product4', 'Vivo')
+            ->with('Product5', 'Oppo')
+            ->with('Product6', 'Asus')
+            ->with('Product7', 'LG')
+            ->with('Product8', 'Xiaomi')
+            ->with('Product9', 'Lenovo')
+            ->with('Product10', 'HP');
+    }
 }
+?>
--- resources/views/Product.blade.php
+++ resources/views/Product.blade.php
@@ -1,3 +1,27 @@
-<div>
-    <!-- The only way to do great work is to love what you do. - Steve Jobs -->
-</div>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Product List</title>
+</head>
+<body>
+    <button><a href="/welcome">Back</a></button>
+
+    <h1>Gadget Brands List</h1>
+
+    <ol>
+    <li>{{$Product1}}</li>
+    <li>{{$Product2}}</li>
+    <li>{{$Product3}}</li>
+    <li>{{$Product4}}</li>
+    <li>{{$Product5}}</li>
+    <li>{{$Product6}}</li>
+    <li>{{$Product7}}</li>
+    <li>{{$Product8}}</li>
+    <li>{{$Product9}}</li>
+    <li>{{$Product10}}</li>
+    </ol>
+    
+</body>
+</html>
